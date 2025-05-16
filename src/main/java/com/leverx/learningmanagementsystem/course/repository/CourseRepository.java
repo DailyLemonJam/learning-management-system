559aa2766f745cdb9bcb3646c03072cfbdb021bc
DailyLemonJam <93944040+DailyLemonJam@users.noreply.github.com>
2025-05-16 14:09:13 +0300

    (Probably) Fix potential lost update error

--- src/main/java/com/leverx/learningmanagementsystem/course/repository/CourseRepository.java
+++ src/main/java/com/leverx/learningmanagementsystem/course/repository/CourseRepository.java
@@ -3,11 +3,14 @@ package com.leverx.learningmanagementsystem.course.repository;
 import com.leverx.learningmanagementsystem.course.model.Course;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
+import static jakarta.persistence.LockModeType.PESSIMISTIC_WRITE;
 
 public interface CourseRepository extends JpaRepository<Course, UUID> {
     List<Course> findAllByCourseSettings_StartDateBetween(LocalDateTime tomorrow, LocalDateTime afterTomorrow);
@@ -15,6 +18,7 @@ public interface CourseRepository extends JpaRepository<Course, UUID> {
     @EntityGraph(attributePaths = {"lessons"})
     List<Course> findAll();
 
+    @Lock(PESSIMISTIC_WRITE)
     @EntityGraph(attributePaths = {"lessons"})
     Optional<Course> findById(UUID id);
 }
