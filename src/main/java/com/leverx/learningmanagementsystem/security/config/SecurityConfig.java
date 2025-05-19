30433bd833f4e4144bf65cb3fe43947428651b99
DailyLemonJam <93944040+DailyLemonJam@users.noreply.github.com>
2025-05-19 16:18:12 +0300

    Create in-memory users for Basic Auth, secure API endpoints locally, configure security for Spring Actuator endpoints locally and allow access for Manager role only, add Spring Security and XSUAA dependencies

---
+++ src/main/java/com/leverx/learningmanagementsystem/security/config/SecurityConfig.java
@@ -0,0 +1,68 @@
+package com.leverx.learningmanagementsystem.security.config;
+
+import com.sap.cloud.security.xsuaa.XsuaaServiceConfiguration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private XsuaaServiceConfiguration xsuaaServiceConfiguration;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var user = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("123"))
+                .roles("USER")
+                .build();
+
+        var admin = User.builder()
+                .username("manager")
+                .password(passwordEncoder().encode("123"))
+                .roles("MANAGER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    @Profile("local")
+    public SecurityFilterChain localSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> {
+                            auth.requestMatchers("/actuator/**").hasRole("MANAGER");
+                            auth.anyRequest().authenticated();
+                        })
+                // TODO: remove "headers..." when move from H2 to Postgres?
+                .headers(headers ->
+                        headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .sessionManagement(configurer ->
+                        configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
