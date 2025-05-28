cf8df4ccd9f6b26d823ae017973dd42668c242d7
DailyLemonJam <93944040+DailyLemonJam@users.noreply.github.com>
2025-05-28 11:23:42 +0300

    Clean up

--- src/main/java/com/leverx/learningmanagementsystem/dto/ErrorResponseDto.java
+++
@@ -1,7 +0,0 @@
-package com.leverx.learningmanagementsystem.dto;
-
-import io.swagger.v3.oas.annotations.media.Schema;
-
-@Schema(description = "Error Response DTO")
-public record ErrorResponseDto(@Schema(description = "Message about error") String message) {
-}
--- src/main/java/com/leverx/learningmanagementsystem/service/email/EmailService.java
+++
@@ -1,5 +0,0 @@
-package com.leverx.learningmanagementsystem.service.email;
-
-public interface EmailService {
-    void sendEmail(String to, String subject, String body);
-}
