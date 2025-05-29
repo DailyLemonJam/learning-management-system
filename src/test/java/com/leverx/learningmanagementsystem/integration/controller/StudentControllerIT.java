0dc56518a99c34d657947b0bd7682e3b3ca08cf4
DailyLemonJam <93944040+DailyLemonJam@users.noreply.github.com>
2025-05-29 13:34:52 +0300

    Fix HANA database dialect

--- src/test/java/com/leverx/learningmanagementsystem/integration/controller/StudentControllerIT.java
+++ src/test/java/com/leverx/learningmanagementsystem/integration/controller/StudentControllerIT.java
@@ -38,7 +38,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@Transactional
 class StudentControllerIT {
     @Autowired
     private MockMvc mockMvc;
