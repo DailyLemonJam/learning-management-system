fa8c342a6c0eaa82985b826f97d7071f0f9d9487
DailyLemonJam <93944040+DailyLemonJam@users.noreply.github.com>
2025-06-16 15:05:53 +0300

    Chore

--- src/main/java/com/leverx/learningmanagementsystem/multitenancy/database/initializer/LocalTenantDataSourceInitializer.java
+++ src/main/java/com/leverx/learningmanagementsystem/multitenancy/database/initializer/LocalTenantDataSourceInitializer.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
@@ -18,6 +19,7 @@ import java.util.List;
 
 @Slf4j
 @Component
+@Profile("local")
 @RequiredArgsConstructor
 public class LocalTenantDataSourceInitializer implements ApplicationRunner {
 
