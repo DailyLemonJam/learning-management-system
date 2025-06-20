5f62133c37a8a38f0c022982990803a85f07f073
DailyLemonJam <93944040+DailyLemonJam@users.noreply.github.com>
2025-06-20 17:46:42 +0300

    Add converter with setLocalScopeAsAuthorities(true)

--- src/main/java/com/leverx/learningmanagementsystem/core/security/config/CloudSecurityConfiguration.java
+++ src/main/java/com/leverx/learningmanagementsystem/core/security/config/CloudSecurityConfiguration.java
@@ -10,6 +10,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -19,6 +21,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -88,8 +91,14 @@ public class CloudSecurityConfiguration {
                 })
                 .oauth2ResourceServer(oauth2 ->
                         oauth2.jwt(jwt ->
-                                jwt.jwtAuthenticationConverter(new TokenAuthenticationConverter(xsuaaServiceConfiguration))))
+                                jwt.jwtAuthenticationConverter(getJwtAuthoritiesConverter())))
                 .build();
+    }
+
+    private Converter<Jwt, AbstractAuthenticationToken> getJwtAuthoritiesConverter() {
+        var converter = new TokenAuthenticationConverter(xsuaaServiceConfiguration);
+        converter.setLocalScopeAsAuthorities(true);
+        return converter;
     }
 
     @Bean
